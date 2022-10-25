642a68e0a4e860068893fb914870e65a8c1584ce
AhmedOmar2017 <eng.ahmed.omar2017@gmail.com>
2022-10-25 07:18:54 +0200

    linked list delete first node and Length

--- Linked_list/LinkedList.c
+++ Linked_list/LinkedList.c
@@ -19,3 +19,56 @@ Node *Append(Node *head, DATA_TYPE element){
     }
     return new_node;
 }
+
+
+nt Length(Node *head){
+    int len = 1;
+    if(head == NULL){   // linked list is embty
+        return 0;
+    }   
+    Node *temp = head;
+    while (temp->next != NULL){
+        len++;
+        temp = temp->next;
+    }
+    return len;
+}
+
+
+int Get_element(Node *head, int node_num, DATA_TYPE *element){
+    if(head == NULL){
+        return 0;
+    }
+    if((node_num > Length(head)) || (node_num < 1)){
+        return -1;
+    }
+
+    Node * temp = head;
+    int x = 1;
+
+    while (x < node_num){
+        temp = temp->next;
+        x++;
+    }
+    *element = temp->element;
+    return 1;
+}
+
+
+
+Node *Delete_First_Node(Node * head){
+    Node *temp;
+    if(head == NULL){
+        return NULL;
+    }
+    if(Length(head) == NULL){
+        free(head);
+        return NULL;     
+    }else{
+        temp = head->next;
+        free(head);
+        return temp; 
+    }
+
+    temp = head->next;
+}
--- Linked_list/LinkedList.h
+++ Linked_list/LinkedList.h
@@ -18,7 +18,7 @@ Node *head;
 Node *Append(Node *, DATA_TYPE);           // Add a new node.
 int Length(Node *);                        // Calculate the numbers of elements in linked list.
 int Get_element(Node *, int, DATA_TYPE *); // get element in the linked list.
-Node Delete_First_Node(Node *);            // Only delete the first element.
+Node *Delete_First_Node(Node *);            // Only delete the first element.
 int Delete_Node(Node *, int);              // Delete any node axcept the first element.
 Node *Insert_First(Node *, DATA_TYPE);     // Only insert the frist element.
 int Insert_After(Node *, int, DATA_TYPE);  // Insert any element except the first element.
